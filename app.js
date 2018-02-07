9d8213f9170751cc1f2a001df588e7fea4d9db51
witflash <witflash@gmail.com>
2018-02-07 10:52:43 +0200

    add cart amount

--- app.js
+++ app.js
@@ -1,42 +1,90 @@
-////////////////////
-//    GET JSON    //
-////////////////////
+'use strict';
+// @TODO
+// LocalStorage
+// if not get than 0, if set than set key
+// add expired
+var cartCount = 0;
+var cart = {};
 
-var request = new XMLHttpRequest();
-request.open('GET', 'data.json', true);
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-		var data = JSON.parse(request.responseText);
-		
-  } else {
-		// We reached our target server, but it returned an error
-  }
-	data.cats.forEach(cat => {
-		let newCat = createCat(cat);
-		document.querySelector('.catalog').appendChild(newCat);
-	});
-};
-request.onerror = function() {
-};
-request.send();
+// @TODO
+// show object as a string
+// cart.toString
 
-
-//////////////////////////
-//    BUILD CAT ITEM    //
-//////////////////////////
-
-function createCat(data) {
-	let template = document.getElementById('cat-wrapper').children[0].cloneNode(true);
-
-	template.querySelector('.cat__name').innerHTML = data.name;
-	template.querySelector('.cat__category').innerHTML = data.category;
-	template.querySelector('.cat__price').innerHTML = data.price;
-	template.querySelector('.cat__photo').children[0].setAttribute("src", data.img_url);
-
-	return template;
+/**
+ * Request data from json file 
+ * @param {Function} cb - add items on page
+ */
+function requestItems(cb) {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'data.json', true);
+	request.onload = function () {
+		if (request.status >= 200 && request.status < 400) {
+			var data = JSON.parse(request.responseText);
+			cb(data);
+		} else {
+		}
+	};
+	request.onerror = function () {
+	};
+	request.send();
 }
 
+function createItems(data) {
+	data.cats.forEach((cat,index) => {
+		let newCat = createItem(cat, index);
+		document.querySelector('.catalog').appendChild(newCat);
+	});
+}
+
+function createItem(data, index) {
+	let item = document.getElementById('cat-template').children[0].cloneNode(true);
+
+	item.querySelector('.cat__name').innerHTML = data.name;
+	item.querySelector('.cat__category').innerHTML = data.category;
+	item.querySelector('.cat__price').innerHTML = data.price;
+	item.querySelector('.cat__img').setAttribute("src", data.img_url);
+	item.setAttribute("data-index", index);
+
+	item.addEventListener('click', handleClick);
+
+	return item;
+}
+
+function handleClick() {
+	var index = this.getAttribute('data-index');
+	addToCart(index);
+}
+
+function addToCart(index) {
+	// increase cart count
+	cartCount++;
+	document.querySelector('.js-cart-amount').textContent = cartCount;
+	index = '#' + index;	
+	if (cart[index]) {
+		cart[index]++;
+	} else {
+		cart[index] = 1;
+	};	
+}
+
+function cartOpen() {
+	// @TODO
+	// add modal window with nice view
+	alert(cart);
+}
+
+// @TODO learn how to write eventListener (iLya)
+//eventListening('click', init.cartOpen(){});
+//document.addEventListener('click',cartOpen());
+document.querySelector('.js-cart-open').addEventListener('click', cartOpen);
+
+requestItems(createItems);
+
+// @TODO
+// add template for cart items
+// with increase count/decrease count/remove (+)(x)
+// input type="number"
+// BEST: custom (-)______(+) (x)
 
 
 
--- tmp/app.js
+++ tmp/app.js
@@ -1,47 +1,87 @@
-(function ($, window, document) {
+'use strict';
+// @TODO
+// LocalStorage
+// if not get than 0, if set than set key
+// add expired
+var cartCount = 0;
+var cart = {};
 
-	$(function () {
-	  menu.init();
-	});
-  
-	var menu = {
-	  $el : $('.js-menu'),
-	  triggerClass: '.js-menu-trigger',
-	  init: function() {
-		if (!this.$el.length) return;
-		$(this.triggerClass).on('click', this.toggle);
-		this.clone();
-	  },
-	  toggle: function() {
-		$('html').toggleClass('menu-open');
-		// $(document).trigger("menu:open");
-	  },
-	  clone: function() {
-		// @todo: clone all elements
-		// dynamic markup
-		// sort by data-menu-order
-		var menuMobile = '.js-mobile-menu',
-			menuItems = '.js-menu-item',
-			wrapper = 'menu-m__item';
-  
-		$(menuItems).clone()
-					.sort(compare)
-					.removeClass()
-					.addClass(wrapper)
-					.appendTo(menuMobile);
-  
-		function compare(a, b) {
-		  return ($(a).data('menu-order') - $(b).data('menu-order'));
+// @TODO
+// show object as a string
+// cart.toString
+
+/**
+ * Request data from json file 
+ * @param {Function} cb - add items on page
+ */
+function requestItems(cb) {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'data.json', true);
+	request.onload = function () {
+		if (request.status >= 200 && request.status < 400) {
+			var data = JSON.parse(request.responseText);
+			cb(data);
+		} else {
 		}
-		// var itemMobile = '<div class="menu-m__item">' + $(this).html() + '</div>';
-		
-		
-  
-	  }
-	}
-  
-	// $(document).on('menu:open', function(){
-	//   console.log('MAGIC WOW');
-	// });
-	
-  }(window.jQuery, window, document));
+	};
+	request.onerror = function () {
+	};
+	request.send();
+}
+
+function createItems(data) {
+	data.cats.forEach((cat,index) => {
+		let newCat = createItem(cat, index);
+		document.querySelector('.catalog').appendChild(newCat);
+	});
+}
+
+function createItem(data, index) {
+	let item = document.getElementById('cat-template').children[0].cloneNode(true);
+
+	item.querySelector('.cat__name').innerHTML = data.name;
+	item.querySelector('.cat__category').innerHTML = data.category;
+	item.querySelector('.cat__price').innerHTML = data.price;
+	item.querySelector('.cat__img').setAttribute("src", data.img_url);
+	item.setAttribute("data-index", index);
+
+	item.addEventListener('click', handleClick);
+
+	return item;
+}
+
+function handleClick() {
+	var index = this.getAttribute('data-index');
+	addToCart(index);
+}
+
+function addToCart(index) {
+	// increase cart count
+	cartCount++;
+	document.querySelector('.js-cart-amount').textContent = cartCount;
+	index = '#' + index;	
+	if (cart[index]) {
+		cart[index]++;
+	} else {
+		cart[index] = 1;
+	};	
+}
+
+function cartOpen() {
+	// @TODO
+	// add modal window with nice view
+	alert(cart);
+}
+
+// @TODO learn how to write eventListener (iLya)
+//eventListening('click', init.cartOpen(){});
+//document.addEventListener('click',cartOpen());
+document.querySelector('.js-cart-open').addEventListener('click', cartOpen);
+
+requestItems(createItems);
+
+// @TODO
+// add template for cart items
+// with increase count/decrease count/remove (+)(x)
+// input type="number"
+// BEST: custom (-)______(+) (x)
